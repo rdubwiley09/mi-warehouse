e4d722c0360809c79413524f1841caa425bff203
Ryan Wiley <wileyrya@gmail.com>
2024-07-08 04:43:38 +0000

    Built intermediate model for vote by candidate

---
+++ mi_warehouse_dbt/models/intermediate/mi_elections/int_vote_by_candidate.sql
@@ -0,0 +1,49 @@
+WITH vote AS (
+    SELECT
+        election_year,
+        election_type,
+        office_code,
+        district_code,
+        status_code,
+        candidate_id,
+        county_code,
+        city_town_code,
+        ward_number,
+        precinct_number,
+        precinct_label,
+        precinct_votes
+    FROM {{ ref('stg_vote')}}
+),
+candidate AS (
+    SELECT
+        election_year,
+        candidate_id,
+        candidate_last_name,
+        candidate_first_name,
+        candidate_middle_name,
+        candidate_party_name
+    FROM {{ ref('stg_name')}}
+),
+output AS (
+    SELECT
+        vote.election_year,
+        election_type,
+        office_code,
+        district_code,
+        status_code,
+        vote.candidate_id,
+        candidate_last_name,
+        candidate_first_name,
+        candidate_middle_name,
+        candidate_party_name,
+        county_code,
+        city_town_code,
+        ward_number,
+        precinct_number,
+        precinct_label,
+        precinct_votes
+    FROM  vote
+    JOIN candidate
+    ON vote.election_year = candidate.election_year AND vote.candidate_id = candidate.candidate_id
+)
+SELECT * FROM output
