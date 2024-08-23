77bdd2d3e6f2264908bf82ecc49ef810751f2334
Ryan Wiley <wileyrya@gmail.com>
2024-08-23 20:31:43 +0000

    Changed legiscan script to use pylegiscan library

--- get_legiscan_data.py
+++ get_legiscan_data.py
@@ -6,28 +6,23 @@ from pathlib import Path
 import zipfile
 from zipfile import ZipFile
 
-import requests
+import requests 
+from pylegiscan.pylegiscan import (
+    get_legiscan_json,
+    convert_pylegiscan_json
+)
+
+GET_LEGISCAN_JSON = False
 
 legiscan_key = os.environ['LEGISCAN_API_KEY']
-TEST_URL = f"https://api.legiscan.com/?key={legiscan_key}&op=getDataset&access_key=1K1zVhV8Epf8hjGNpECuUL&id=2027"
+access_key = "3qrkh1Mb5Hgg0K5a7z85RN&id=2027"
 
-response = requests.get(TEST_URL, stream=True)
-data = response.json()
-zip_data = data['dataset']['zip']
-base64_bytes = zip_data.encode('ascii')
-message_bytes = base64.b64decode(base64_bytes)
-with zipfile.ZipFile(BytesIO(message_bytes)) as zf:
-    files = zf.namelist()
-    for file in files:
-        split = file.split("/")
-        if len(split) != 4:
-            continue
-        legislature = split[1]
-        file_type = split[2]
-        filename = split[3]
-        needed_loc = f'./data/raw/legiscan/{legislature}/{file_type}'
-        Path(needed_loc).mkdir(parents=True, exist_ok=True)
-        with zf.open(file) as f:
-            out_dict = json.loads(f.read())
-            with open(f'{needed_loc}/{filename}', 'w') as g:
-                json.dump(out_dict, g)
+if GET_LEGISCAN_JSON:
+    get_legiscan_json(legiscan_key, access_key, './data/raw/legiscan')
+
+convert_pylegiscan_json(
+    './data/raw/legiscan/2023-2024_102nd_Legislature/people',
+    './data/raw/legiscan/2023-2024_102nd_Legislature/bill',
+    './data/raw/legiscan/2023-2024_102nd_Legislature/vote',
+    './data/raw/legiscan/2023-2024_102nd_Legislature/parsed',
+)
